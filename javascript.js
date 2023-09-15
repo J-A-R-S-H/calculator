4022a7413dcf90181b9930536ad56b621ef24bf5
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2023-09-15 14:52:50 -0700

    calculator skeleton

---
+++ javascript.js
@@ -0,0 +1,35 @@
+
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+
+
+function operate(firstNum, operator, secondNum) {
+    if (operator = "+") {
+        return add(firstNum, secondNum)
+    }
+    else if (operator = "-") {
+        return subtract(firstNum, secondNum)
+    }
+    else if (operator = "*") {
+        return multiply(firstNum, secondNum)
+    }
+    else if (operator = "/") {
+        return divide(firstNum, secondNum)
+    }
+}
