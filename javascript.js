8a17cb4ca0081c4e375e6b7081fd0c344eaea06a
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2023-09-16 20:49:03 -0700

    clear working

--- javascript.js
+++ javascript.js
@@ -9,10 +9,10 @@ const previousDisplayNumber = document.querySelector(".previousNumber")
 
 
 const equal = document.querySelector(".equal")
-equal.addEventListener("click", () =>{
-if(currentNum != "" && previousNum != ""){
-calculate()
-}
+equal.addEventListener("click", () => {
+    if (currentNum != "" && previousNum != "") {
+        calculate()
+    }
 })
 
 
@@ -20,7 +20,7 @@ const decimal = document.querySelector(".decimal")
 
 
 const clear = document.querySelector(".clear")
-
+clear.addEventListener("click", clearCalculator)
 
 const numberButtons = document.querySelectorAll(".number")
 
@@ -29,84 +29,92 @@ const operators = document.querySelectorAll(".operator")
 
 
 numberButtons.forEach((btn) => {
-btn.addEventListener("click", (e) => {
-handleNumber(e.target.textContent)
+    btn.addEventListener("click", (e) => {
+        handleNumber(e.target.textContent)
+    });
 });
-});
 
 
 
 
-function handleNumber(number){
-if (currentNum.length <= 11){
-currentNum += number
-currentDisplayNumber.textContent = currentNum
-}
+function handleNumber(number) {
+    if (currentNum.length <= 11) {
+        currentNum += number
+        currentDisplayNumber.textContent = currentNum
+    }
 }
 
 
 operators.forEach(btn => {
-btn.addEventListener("click", (e) =>
-handleOperator(e.target.textContent)
-)
+    btn.addEventListener("click", (e) =>
+        handleOperator(e.target.textContent)
+    )
 })
 
 
-function handleOperator(op){
-operator = op;
-previousNum = currentNum;
-previousDisplayNumber.textContent = previousNum + "" + operator
-currentNum = ""
-currentDisplayNumber.textContent = ""
+function handleOperator(op) {
+    operator = op;
+    previousNum = currentNum;
+    previousDisplayNumber.textContent = previousNum + "" + operator
+    currentNum = ""
+    currentDisplayNumber.textContent = ""
 }
 
 
-function calculate(){
-previousNum = Number(previousNum)
-current = Number(currentNum)
+function calculate() {
+    previousNum = Number(previousNum)
+    current = Number(currentNum)
 
 
-if (operator === "+"){
-previousNum = previousNum += currentNum
-}
-else if (operator === "-"){
-previousNum = previousNum -= currentNum
-}
+    if (operator === "+") {
+        previousNum = previousNum += currentNum
+    }
+    else if (operator === "-") {
+        previousNum = previousNum -= currentNum
+    }
 
 
-else if (operator === "x"){
-previousNum = previousNum *= currentNum
-}
+    else if (operator === "x") {
+        previousNum = previousNum *= currentNum
+    }
 
 
-else if (operator === "/"){
-if (currentNum <= 0){
-previousNum = "Error"
-previousDisplayNumber.textContent = "";
-displayResults()
-return
-}
-previousNum /= currentNum
-}
-previousNum = roundNumber(previousNum)
-previousNum = previousNum.toString()
-displayResults()
-}
-
-
-function roundNumber (num){
-return Math.round(num * 100000) / 100000
+    else if (operator === "/") {
+        if (currentNum <= 0) {
+            previousNum = "Error"
+            previousDisplayNumber.textContent = "";
+            displayResults()
+            return
+        }
+        previousNum /= currentNum
+    }
+    previousNum = roundNumber(previousNum)
+    previousNum = previousNum.toString()
+    displayResults()
 }
 
 
-function displayResults(){
-previousDisplayNumber.textContent = ""
-currentDisplayNumber.textContent = previousNum
-operator = ""
-if(previousNum.length <= 11){
-currentDisplayNumber.textContent = previousNum
+function roundNumber(num) {
+    return Math.round(num * 100000) / 100000
 }
-else {
-currentDisplayNumber.textContent = previousNum.slice(0, 11) + "..."
+
+
+function displayResults() {
+    previousDisplayNumber.textContent = ""
+    currentDisplayNumber.textContent = previousNum
+    operator = ""
+    if (previousNum.length <= 11) {
+        currentDisplayNumber.textContent = previousNum
+    }
+    else {
+        currentDisplayNumber.textContent = previousNum.slice(0, 11) + "..."
+    }
 }
+
+function clearCalculator() {
+    currentNum = ""
+    previousNum = ""
+    operator = ""
+    currentDisplayNumber.textContent = "0"
+    previousDisplayNumber.textContent = ""
 }
